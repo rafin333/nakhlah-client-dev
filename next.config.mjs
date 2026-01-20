0757ab36a2430bb35a4ccd6067ff16989473178b
rafin333 <mashrur.rahman333@gmail.com>
2026-01-20 20:55:10 +0600

    v0.0.9 overall changes

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,15 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'api.nakhlah.xyz',
+        hostname: 'api.siamrtx.space',
       },
       {
         protocol: 'https',
-        hostname: 'devapi.nakhlah.xyz',
+        hostname: 'devapi.siamrtx.space',
+      },
+      {
+        protocol: 'https',
+        hostname: 'testapi.siamrtx.space',
       },
       {
         protocol: 'https',
